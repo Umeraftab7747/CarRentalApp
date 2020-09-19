c3b1cea0e01eded936f86be856a622d2d2d2b78f
Umeraftab7747 <umeraftab7747@gmail.com>
2020-09-19 02:10:02 -0700

    flatlistContainer

--- src/screens/dashboard/Dashboard.js
+++ src/screens/dashboard/Dashboard.js
@@ -14,16 +14,11 @@ import {
   heightPercentageToDP as h,
 } from 'react-native-responsive-screen';
 
+// animation libary
+import {AnimatedFlatList, AnimationType} from 'flatlist-intro-animations';
+
 // nativebase
-import {
-  DeckSwiper,
-  Card,
-  CardItem,
-  Thumbnail,
-  Left,
-  Body,
-  Icon,
-} from 'native-base';
+import {DeckSwiper, Card, CardItem} from 'native-base';
 
 // Components
 import {Appbtn, Apptxt, Navheader, Appsearch} from '../../components';
@@ -70,7 +65,7 @@ export class Dashboard extends Component {
         backgroundColor: 'red',
       }}>
       <CardItem cardBody>
-        <Image style={{height: h('40%'), flex: 1}} source={item.image} />
+        <Image style={{height: h('35%'), flex: 1}} source={item.image} />
       </CardItem>
       <CardItem
         style={{
@@ -96,11 +91,44 @@ export class Dashboard extends Component {
         style={{
           justifyContent: 'center',
           alignItems: 'center',
+          height: h('3%'),
+        }}>
+        <Text style={{color: '#0007', fontSize: h('2.5%')}}>Stars</Text>
+      </CardItem>
+      <CardItem
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
           height: h('9%'),
         }}>
         <Appbtn txt={'BOOK'} />
       </CardItem>
     </Card>
+  );
+
+  // flatlist
+  Flatitem = (item) => (
+    <View style={styles.FlatlistContianer}>
+      <View style={styles.itemlistView}>
+        <View style={styles.left}>
+          <Text style={styles.name}>{item.name}</Text>
+          <Text style={styles.text}>{item.text}</Text>
+          <Text style={styles.Star}>Stars {item.Star}</Text>
+        </View>
+
+        <View style={styles.right}>
+          <Image source={item.image} style={styles.FlatlistimgContainers} />
+        </View>
+      </View>
+      <View
+        style={{
+          // marginTop: h('1%'),
+          // backgroundColor: 'red',
+          width: '100%',
+          height: h('1.5%'),
+        }}
+      />
+    </View>
   );
 
   render() {
@@ -118,11 +146,19 @@ export class Dashboard extends Component {
         {/* lower */}
         <View style={styles.lowerContainer}>
           {/* decker */}
-          <Text style={styles.txt}>Choose your car</Text>
+          {/* <Text style={styles.txt}>Choose your car</Text>
           <DeckSwiper
             dataSource={this.state.card}
             renderItem={(item) => this.renderItemCard(item)}
+          /> */}
+
+          <AnimatedFlatList
+            data={this.state.card}
+            renderItem={({item}) => this.Flatitem(item)}
+            animationType={AnimationType.Dive}
+            animationDuration={1000}
           />
+
           {/* end */}
         </View>
       </View>
@@ -157,5 +193,42 @@ const styles = StyleSheet.create({
     fontSize: h('3%'),
     marginLeft: h('20%'),
     marginTop: h('1%'),
+  },
+  itemlistView: {
+    backgroundColor: 'white',
+    width: '90%',
+    height: h('15%'),
+    margin: h('1%'),
+    flexDirection: 'row',
+    elevation: h('1%'),
+  },
+  FlatlistContianer: {
+    // justifyContent: 'center',
+    alignItems: 'center',
+  },
+  left: {
+    // backgroundColor: 'tomato',
+    width: '70%',
+    height: '100%',
+    // alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: h('3%'),
+  },
+  right: {
+    // backgroundColor: 'red',
+    width: '30%',
+  },
+  name: {
+    color: 'black',
+    fontSize: h('3%'),
+    fontWeight: 'bold',
+  },
+  text: {
+    color: '#0006',
+    fontSize: h('2%'),
+  },
+  FlatlistimgContainers: {
+    width: '100%',
+    height: '100%',
   },
 });
